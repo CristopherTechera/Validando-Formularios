f59cdc0724782f08299a82c3f3bcd459da22da07
CristopherTechera <122094255+CristopherTechera@users.noreply.github.com>
2023-03-14 16:10:50 -0300

    Add files via upload
    Sustituyendo los archivos que hemos terminado

--- js/validaciones.js
+++ js/validaciones.js
@@ -3,11 +3,74 @@ export function valida( input ){
         if(validadores[tipoDeInput]){
             validadores[tipoDeInput](input);
         }
+        if(input.validity.valid){
+            input.parentElement.classList.remove("input-container--invalid");
+            input.parentElement.querySelector(".input-message-error").innerHTML = "";
+        }else{
+            input.parentElement.classList.add("input-container--invalid");
+            input.parentElement.querySelector(".input-message-error").innerHTML = 
+            mostrarMensajeDeError(tipoDeInput, input);
+        }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+]
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo nombre no puede estar vacío",
+    },
+    email: {
+        valueMissing: "El campo correo no puede estar vacío",
+        typeMismatch: "El correo no es válido",
+    },
+    password: {
+        valueMissing: "El campo password no puede estar vacío",
+        patternMismatch: "Al menos 6 carateres, máximo 12, debe tener una letra mayúscula, una minúscula, un número y no puede contener caracteres especiales.",
+    },
+    nacimiento: {
+        valueMissing: "El campo fecha de nacimiento no puede estar vacío",
+        customError: "Debes tener al menos 18 años de edad.",
+    },
+    telefono: {
+        valueMissing: "El campo número telefónico no puede estar vacío",
+        patternMismatch: "El formato requerido es xxxxxxxxxx 10 números",
+    },
+    direccion: {
+        valueMissing: "El campo número telefónico no puede estar vacío",
+        patternMismatch: "La dirección debe contener entre 10 y 40 caracteres",
+    },
+    ciudad: {
+        valueMissing: "El campo número telefónico no puede estar vacío",
+        patternMismatch: "La ciudad debe contener entre 3 y 40 caracteres",
+    },
+    estado: {
+        valueMissing: "El campo número telefónico no puede estar vacío",
+        patternMismatch: "El campo estado debe contener entre 3 y 40 caracteres",
+    },
 }
 
 const validadores = {
     nacimiento: (input) => validarNacimiento(input),
 };
+
+function mostrarMensajeDeError(tipoDeInput, input){
+    let mensaje = "";
+    tipoDeErrores.forEach( error => {
+        if(input.validity[error]){
+            console.log(input.validity[error]);
+            console.log(mensajesDeError[tipoDeInput][error]);
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    } )
+
+    return mensaje;
+}
+
 
 function validarNacimiento(input){
     const fechaCliente = new Date (input.value);
